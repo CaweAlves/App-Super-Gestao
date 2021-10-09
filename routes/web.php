55af9e691c560909fdd670d515e2edc5a9840575
Cawe Alves <Cawe.wewe04@gmail.com>
2021-10-09 01:30:09 -0300

    Implementando rotas de login

--- routes/web.php
+++ routes/web.php
@@ -68,6 +68,8 @@ Route::middleware('autenticacao:padrao, visitante')->prefix('/app')->group(funct
     Route::get('/sair', [LoginController::class, 'sair'])->name('app.sair');
     Route::get('/cliente', [ClienteController::class, 'index'])->name('app.cliente');
     Route::get('/fornecedor', [FornecedorController::class, 'index'])->name('app.fornecedor');
+    Route::post('/fornecedor/listar', [FornecedorController::class, 'listar'])->name('app.fornecedor.listar');
+    Route::get('/fornecedor/adicionar', [FornecedorController::class, 'adicionar'])->name('app.fornecedor.adicionar');
     Route::get('/produto', [ProdutoController::class, 'index'])->name('app.produto');
 });
 
