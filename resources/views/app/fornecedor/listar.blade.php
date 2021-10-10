38ff58419f7551d630355da7c52b3402db498976
Cawe Alves <Cawe.wewe04@gmail.com>
2021-10-09 22:38:56 -0300

    implementando listangem de fornecedores (read)

--- resources/views/app/fornecedor/listar.blade.php
+++ resources/views/app/fornecedor/listar.blade.php
@@ -16,8 +16,33 @@
         </div>
 
         <div class="informacao-pagina">
-            <div style="width: 30%; margin-left: auto; margin-right: auto;">
-                ...lista...
+            <div style="width: 90%; margin-left: auto; margin-right: auto;">
+                <table border="1" width="100%">
+                    <thead>
+                        <tr>
+                        <th>Nome</th>
+                        <th>Site</th>
+                        <th>UF</th>
+                        <th>Email</th>
+                        <th>Excluir</th>
+                        <th>Editar</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>    
+                        
+                        @foreach ($fornecedores as $fornecedor)
+                            <tr>
+                                <th>{{ $fornecedor->nome }}</th>
+                                <th>{{ $fornecedor->site }}</th>
+                                <th>{{ $fornecedor->uf }}</th>
+                                <th>{{ $fornecedor->email }}</th>
+                                <th>Excluir</th>
+                                <th>Editar</th>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
         </div>
 </div>
 @endsection
