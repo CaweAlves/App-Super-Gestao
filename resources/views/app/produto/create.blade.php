72d0cad34266be1914f6d417dd6873d33ab9d295
Cawe Alves <Cawe.wewe04@gmail.com>
2021-10-14 13:33:24 -0300

    Add paginas de produtos e arrumando seus links

---
+++ resources/views/app/produto/create.blade.php
@@ -0,0 +1,41 @@
+@extends('app.layouts.basico')
+
+@section('titulo', 'Produto')
+
+@section('conteudo')
+    
+    <div class="conteudo-pagina">
+        <div class="titulo-pagina-2">
+            <p>Adicionar Produto </p>
+        </div>
+
+        <div class="menu">
+        <ul>
+        <li><a href=" {{ route('produto.index')}} ">Voltar</a></li>
+        <li><a href=" {{ route('app.fornecedor')}} ">Consulta</a></li>
+        </div>
+
+        <div class="informacao-pagina"> 
+            <div style="width: 30%; margin-left: auto; margin-right: auto;">
+                <form method="post" action="">
+                @csrf
+                    <input type="text" name="nome"  placeholder="Nome" class="borda-preta">
+                    
+                    <input type="text" name="descricao"  placeholder="descricao" class="borda-preta"> 
+                    
+                    <input type="text" name="peso"  placeholder="peso" class="borda-preta">
+                    
+                    <select name="unidade_id">
+                        <option>-- Selecione a Unidade de Medida </option>
+
+                        @foreach ($unidades as $unidade )
+                            <option value=" {{ $unidade->id }} ">Unidade</option>
+                        @endforeach
+                        
+                    </select>
+
+                    <button type="submit" class="borda-preta">Cadastrar</button>
+            </form>
+        </div>
+</div>
+@endsection
---
+++ resources/views/app/produto/index.blade.php
@@ -0,0 +1,64 @@
+@extends('app.layouts.basico')
+
+@section('titulo', 'Produto')
+
+@section('conteudo')
+    
+    <div class="conteudo-pagina">
+        <div class="titulo-pagina-2">
+            <p>Listagem de Produtos</p>
+        </div>
+
+        <div class="menu">
+        <ul>
+        <li><a href=" {{ route('produto.create')}} ">Novo</a></li>
+        <li><a href=" {{ route('produto.index')}} ">Consulta</a></li>
+        </div>
+
+        <div class="informacao-pagina">
+            <div style="width: 90%; margin-left: auto; margin-right: auto;">
+                <table border="1" width="100%">
+                    <thead>
+                        <tr>
+                        <th>Nome</th>
+                        <th>Descrição</th>
+                        <th>Peso</th>
+                        <th>unidade ID</th>
+                        <th></th>
+                        <th></th>
+                        </tr>
+                    </thead>
+
+                    <tbody>    
+                        
+                        @foreach ($produtos as $produto)
+                            <tr>
+                                <td>{{ $produto->nome }}</td>
+                                <td>{{ $produto->descricao }}</td>
+                                <td>{{ $produto->peso }}</td>
+                                <td>{{ $produto->unidade_id }}</td>
+                                <td><a href="">Excluir</a></td>
+                                <td><a href="">Editar</a></td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+                {{ $produtos->appends($request)->links() }}
+                <br>
+
+                Exibindo  {{ $produtos->count() }} produtos de {{ $produtos->total() }} 
+                    (de {{ $produtos->firstItem() }} a {{ $produtos->LastItem() }} )
+{{-- 
+                {{ $fornecedores->count() }} - Total de registros por página
+                <br>
+                {{ $fornecedores->total() }} - Total de registros da consulta
+                <br>
+                {{ $fornecedores->firstItem() }} - Número do primeiro registro da página
+                <br>
+                {{ $fornecedores->firstItem() }} - Número do primeiro registro da página
+                <br>
+                {{ $fornecedores->LastItem() }} - Número do último registro da página --}}
+
+        </div>
+</div>
+@endsection
