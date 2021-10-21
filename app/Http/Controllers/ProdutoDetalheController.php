84cbc3d3e6649571ef6e2304c3357328404dd5f9
Cawe Alves <Cawe.wewe04@gmail.com>
2021-10-21 13:10:40 -0300

    Add componente de form e implementando create

--- app/Http/Controllers/ProdutoDetalheController.php
+++ app/Http/Controllers/ProdutoDetalheController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Unidade;
+use App\Models\ProdutoDetalhe;
 
 class ProdutoDetalheController extends Controller
 {
@@ -23,7 +25,9 @@ class ProdutoDetalheController extends Controller
      */
     public function create()
     {
-        //
+        $unidades = Unidade::all();
+
+        return view('app.produto_detalhe.create', ['unidades' => $unidades]);
     }
 
     /**
@@ -34,7 +38,8 @@ class ProdutoDetalheController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        ProdutoDetalhe::create($request->all());
+        echo ' Cadastro realizado com sucesso';
     }
 
     /**
@@ -56,7 +61,8 @@ class ProdutoDetalheController extends Controller
      */
     public function edit($id)
     {
-        //
+        
+        return view('app.produto_detalhe.edit');
     }
 
     /**
---
+++ resources/views/app/produto_detalhe/_components/form_create_edit.blade.php
@@ -0,0 +1,32 @@
+@if(isset($produto_detalhe->id))
+    <form method="post" action=" {{ route('produto-detalhe.update', ['produto' => $produto_detalhe->id])}} ">
+        @csrf
+        @method('PUT')
+@else
+    <form method="post" action=" {{ route('produto-detalhe.store')}} ">
+        @csrf
+@endif
+        <input type="text" name="produto_id" value="{{ $produto_detalhe->produto_id ?? old('produto_id') }}"  placeholder="produto_id" class="borda-preta">
+            {{ $errors->has('produto_id') ? $errors->first('produto_id') : '' }}
+        
+        <input type="text" name="comprimento" value="{{ $produto_detalhe->comprimento ?? old('comprimento')}}"  placeholder="comprimento" class="borda-preta"> 
+            {{ $errors->has('comprimento') ? $errors->first('comprimento') : '' }}
+
+        <input type="text" name="largura" value="{{ $produto_detalhe->largura ?? old('largura')}}"  placeholder="largura" class="borda-preta">
+            {{ $errors->has('largura') ? $errors->first('largura') : '' }}
+        
+        <input type="text" name="altura" value="{{ $produto_detalhe->altura ?? old('altura')}}"  placeholder="altura" class="borda-preta">
+            {{ $errors->has('altura') ? $errors->first('altura') : '' }}
+        
+        <select name="unidade_id" >
+            <option>-- Selecione a Unidade de Medida </option>
+
+            @foreach ($unidades as $unidade )
+                <option value="{{$unidade->id}}" {{ $produto_detalhe->unidade_id ?? old('unidade_id') == $unidade->id ? 'selected' : '' }}>Unidade</option>
+            @endforeach
+    
+        </select>
+            {{ $errors->has('unidade_id') ? $errors->first('unidade_id') : '' }}
+
+        <button type="submit" class="borda-preta">Cadastrar</button>
+    </form>
