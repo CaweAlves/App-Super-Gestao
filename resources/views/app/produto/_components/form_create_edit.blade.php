d8dbbc413330fe01953b53b0f567203acb66bb2d
Cawe Alves <Cawe.wewe04@gmail.com>
2021-10-21 12:47:44 -0300

    Arrumando old do formulario para edit

--- resources/views/app/produto/_components/form_create_edit.blade.php
+++ resources/views/app/produto/_components/form_create_edit.blade.php
@@ -6,20 +6,20 @@
     <form method="post" action=" {{ route('produto.store')}} ">
         @csrf
 @endif
-        <input type="text" name="nome" value="{{ old('nome') }}"  placeholder="Nome" class="borda-preta">
+        <input type="text" name="nome" value="{{ $produto_detalhe->nome ?? old('nome') }}"  placeholder="Nome" class="borda-preta">
             {{ $errors->has('nome') ? $errors->first('nome') : '' }}
         
-        <input type="text" name="descricao" value="{{ old('descricao')}}"  placeholder="descricao" class="borda-preta"> 
+        <input type="text" name="descricao" value="{{ $produto_detalhe->descricao ?? old('descricao')}}"  placeholder="descricao" class="borda-preta"> 
             {{ $errors->has('descricao') ? $errors->first('descricao') : '' }}
 
-        <input type="text" name="peso" value="{{ old('peso')}}"  placeholder="peso" class="borda-preta">
+        <input type="text" name="peso" value="{{ $produto_detalhe->peso ?? old('peso')}}"  placeholder="peso" class="borda-preta">
             {{ $errors->has('peso') ? $errors->first('peso') : '' }}
         
         <select name="unidade_id" >
             <option>-- Selecione a Unidade de Medida </option>
 
             @foreach ($unidades as $unidade )
-                <option value="{{$unidade->id}}" {{ old('unidade_id') == $unidade->id ? 'selected' : '' }}>Unidade</option>
+                <option value="{{$unidade->id}}" {{ $produto_detalhe->unidade_id ?? old('unidade_id') == $unidade->id ? 'selected' : '' }}>Unidade</option>
             @endforeach
     
         </select>
