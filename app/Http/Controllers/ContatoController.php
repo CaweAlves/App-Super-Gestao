46555f285d07e468c05df6a8b9d799713409d96e
Cawe Alves <Cawe.wewe04@gmail.com>
2021-10-26 15:47:18 -0300

    Implementando migrate de pedidos e clientes

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -8,8 +8,9 @@ use App\Models\MotivoContato;
 
 class ContatoController extends Controller
 {
-    public function contato(Request $request){
-        
+    public function contato(Request $request)
+    {
+
         $motivo_contatos = MotivoContato::all();
         // echo '<pre>';
         // print_r($request->all());
@@ -32,43 +33,44 @@ class ContatoController extends Controller
 
         // $contato =  new SiteContato();
         // $contato->create($request->all());
-        
+
         //$contato->save();
         //print_r($contato->getAttributes());
 
-        return view('site.contato', ['titulo' => 'Contato', 'motivo_contatos' => $motivo_contatos]);    
+        return view('site.contato', ['titulo' => 'Contato', 'motivo_contatos' => $motivo_contatos]);
     }
 
-    public function salvar(Request $request){
+    public function salvar(Request $request)
+    {
 
 
-        $regras = 
+        $regras =
             [
-            'nome' => 'required|min:3|max:40|unique:site_contatos',
-            'telefone' => 'required',
-            'email' => 'email',
-            'motivo_contatos_id' => 'required',
-            'mensagem' => 'required|max:2000'
+                'nome' => 'required|min:3|max:40|unique:site_contatos',
+                'telefone' => 'required',
+                'email' => 'email',
+                'motivo_contatos_id' => 'required',
+                'mensagem' => 'required|max:2000'
             ];
 
         $feedbacks =
-        [
-            // FEEDBACKS DE VALIDAÇÕES ESPECIFICOS
-            'nome.min' => 'O campo nome precisa ter no minimo 3 caracteres',
-            'nome.max' => 'A mensagem deve ter no maximo 40 caracteres',
-            'nome.unique' => 'Este nome já está sendo utilizado',
+            [
+                // FEEDBACKS DE VALIDAÇÕES ESPECIFICOS
+                'nome.min' => 'O campo nome precisa ter no minimo 3 caracteres',
+                'nome.max' => 'A mensagem deve ter no maximo 40 caracteres',
+                'nome.unique' => 'Este nome já está sendo utilizado',
 
-            'email.email' => 'O email informado não é valido',
-            
-            'mensagem.max' => 'A mensagem deve ter no maximo 2000 caracteres',
-            
-            // FEEDBACKS DE VALIDAÇÕES GENERICOS
-            'required' => 'O campo :attribute deve ser preenchido'
-        ]; 
+                'email.email' => 'O email informado não é valido',
+
+                'mensagem.max' => 'A mensagem deve ter no maximo 2000 caracteres',
+
+                // FEEDBACKS DE VALIDAÇÕES GENERICOS
+                'required' => 'O campo :attribute deve ser preenchido'
+            ];
 
         // Realizar validação dos formularios recebidos no request
         $request->validate($regras, $feedbacks);
-        
+
         SiteContato::create($request->all());
         return redirect()->route('site.sucesso');
     }
